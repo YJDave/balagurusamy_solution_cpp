61205adc83b98ca9191f61ac092b65f86a50f62f
Yashashvi Dave <yashashvidave@gmail.com>
2017-09-02 19:15:16 +0530

    Complete Exercise 2

---
+++ exercise2/2.3_Acsii.cpp
@@ -0,0 +1,26 @@
+/*
+
+    write a program that inputs a character from keyboard and displays its corresponding
+    ASCII value on the screen.
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char c;
+    int a;
+
+    cout<<"\nEnter character to find it's ASCII value : ";
+    cin>>c;
+    a=c;
+
+    //first method
+    cout<<"\nASCII value for "<<c<<" is "<<a;
+
+    //second method
+    cout<<"\nASCII value for "<<c<<" is "<<(int)c;
+    return 0;
+}
---
+++ exercise2/2.5_ConvertTemprature.cpp
@@ -0,0 +1,22 @@
+/*
+
+    write a C++ program that will ask for a temperature in Fahrenheit and display in Celsius
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    float temp;
+
+    cout<<"\nEnter the temperature in Fahrenheit : ";
+    cin>>temp;
+
+    temp=(temp-32)*5/9;
+
+    cout<<"\nTemperature in Celsius is  : "<<temp<<endl;
+    return 0;
+}
