31f18270b2983982481fce2be763eb03fbb5e665
YJDave <yashashvidave@gmail.com>
2017-12-11 22:16:33 +0530

    5: Add solution for exercise 5.5

---
+++ Exercise_5/5.5_Distance.cpp
@@ -0,0 +1,84 @@
+/*  Create two classes DM and DB which store the values of distance.
+    DM stores distance in meters and centimeters.
+    DB stores distances in feet and inches.
+    Write a program that can read values for the class object and add one
+    object of DM with another object of DB.
+    Use a friend function to carry out the addition operation and this
+    function will display answer in meter and centimeters
+*/
+
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+class DB;
+class DM
+{
+	float meter;
+	float cm;
+public:
+    DM()
+    {
+        meter = 0;
+        cm = 0;
+    }
+    DM(float a, float b)
+    {
+        meter = a;
+        cm = b;
+    }
+    void display(void)
+    {
+        cout<<"\nDistance is meter = "<<meter
+            <<" centimeter = "<<cm<<endl;
+    }
+    friend DM operator +(const DM &, const DB &);
+};
+
+class DB
+{
+	float feet;
+	float inch;
+public:
+
+	DB()
+    {
+        feet = 0;
+        inch = 0;
+    }
+    DB(float a, float b)
+    {
+        feet = a;
+        inch = b;
+    }
+    void display(void)
+    {
+        cout<<"\nDistance is feet = "<<feet
+            <<" inch = "<<inch<<endl;
+    }
+    friend DM operator +(const DM &, const DB &);
+
+};
+
+DM operator +(const DM &d1, const DB &d2)
+{
+    DM d3;
+    d3.meter = d1.meter + d2.feet/3.28;
+    d3.cm = d1.cm + d2.inch/2.54;
+    return d3;
+}
+
+int main()
+{
+    DM d1(89,9);
+    DB d2(70,89);
+    d1.display();
+    d2.display();
+    DM d3;
+    cout<<"\nBy adding these two object of distance ";
+    d3 = d1 + d2;
+    d3.display();
+    return 0;
+
+}
