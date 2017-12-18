cbb1c11b5f2a993d2a796fb2e1ec7b2917cbb1b9
anerimehta <aneri1297@gmail.com>
2017-12-18 22:34:54 +0530

    7: Solution for 7.4

---
+++ Exercise_7/7.4_Strings.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+
+class Str
+{
+	char a[20];
+	
+public:
+	int operator==(Str);
+	void get(void);
+};
+
+void Str::get(void)
+{
+	std::cout<<"Enter your string..";
+	std::cin>>a;
+	std::cout<<a<<"\n";
+}
+
+int Str::operator==(Str s)
+{
+	int i=0,flag=0;
+	while((a[i]!='\0')||(s.a[i]!='\0'))
+	{
+		if(a[i]!=s.a[i])
+		{
+			flag++;
+			break;
+		}
+		i++;
+	}
+	if(flag==0)
+		return(1);
+	else
+		return(0);
+}
+
+int main()
+{
+	Str s1,s2;
+	s1.get();
+	s2.get();
+	if(s1==s2)
+		std::cout<<"Strings match!!\n";
+	else
+		std::cout<<"oops!! Strings don't match..\n";
+}
