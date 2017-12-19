46394441f444a63a73519c05e4789849180fe963
anerimehta <aneri1297@gmail.com>
2017-12-19 18:59:05 +0530

    4: Solution for 4.5

---
+++ Exercise_4/4.5_Macro.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<iostream>
+#define MAX(a, b, c) ((a)>(b)?((a)>(c)?(1):(0)):((b)>(c)?(-1):(0)))//:(a==b)?(0):(-1))
+
+int main()
+{
+	int x,y,z,s;
+	std::cout<<"Enter three numbers: ";
+	std::cin>>x>>y>>z;
+	std::cout<<"\n";
+	s = MAX(x,y,z);
+	if(s==1)
+	std::cout<<"The Maximum of the numbers is: "<<x;
+	else if(s==0)
+	std::cout<<"The Maximum of the numbers is: "<<z;
+	else
+	std::cout<<"The Maximum of the numbers is: "<<y;
+	std::cout<<"\n";
+	return(0);
+}
